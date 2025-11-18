e0fa9c32e46a94449f33b906d8ecde06b6d9c98e
Rashnerd <akhigbek6@gmail.com>
2025-11-18 22:38:59 +0100

    feat: add WaitlistSuccessCard component and integrate it into the landing page; enhance waitlist functionality

---
+++ app/components/WaitlistSuccessCard.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { FaCircleCheck } from "react-icons/fa6";
+
+export const WaitlistSuccessCard = () => {
+    const listOfAvatars = [
+        "/images/stock_avatars/avatar1.png",
+        "/images/stock_avatars/avatar2.png",
+        "/images/stock_avatars/avatar3.png",
+    ]
+  return (
+    // TODO: Revamp the design of this card to make it more appealing
+    <>
+        <div className='flex flex-col'>
+            {/* The Icon */}
+            <div className='flex justify-center mb-6'>
+                <div className='size-16 lg:size-20 rounded-full bg-primary/10 flex items-center justify-center'>
+                    <FaCircleCheck className='size-8 lg:size-10 text-primary' />
+                </div>
+            </div>
+        
+            {/* The header */}
+            <h1 className='font-[800] mb-4 lg:mb-6 text-xl lg:text-3xl leading-[1.3] text-black text-center'>
+                Thank you for joining our 
+                <span className='text-primary'> waitlist!</span>
+            </h1>
+        
+            {/* The statement */}
+            <p className='text-black/70 mb-10 text-center'>
+                We’re excited to have you on board. We'll be in touch with updates and launch details soon.
+            </p>
+        
+            {/* The image collection */}
+            <div className='relative flex flex-col gap-2'>
+                <div className="flex justify-center">
+                    <div className="flex -space-x-3">
+                        {listOfAvatars.map((src) => (
+                            <img
+                                key={src}
+                                src={src}
+                                alt=""                                    className="size-10 rounded-full border-2 border-white object-cover bg-muted flex-shrink-0"
+                            />
+                        ))}
+                    </div>
+                </div>
+                <p className='text-[8px] lg:text-xs text-center'>You're not alone, <span className='text-primary font-bold'>1,500+</span> people joined! </p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default WaitlistSuccessCard;
--- app/routes/landingpage.tsx
+++ app/routes/landingpage.tsx
@@ -1,56 +1,165 @@
 import React from 'react'
 import type { Route } from './+types/landingpage';
 import { FaArrowRightLong } from "react-icons/fa6";
+import { Form, redirect } from 'react-router';
 
 // Component for the landing page
 import Navbar from '~/components/navbar';
 import { MailMinus } from 'lucide-react';
+import { request } from 'http';
+import WaitlistSuccessCard from '~/components/WaitlistSuccessCard';
+import Footer from '~/components/Footer';
 
-export const LandingPage = () => {
+export const meta = () => {
+  return [
+    { title: "Campex - Your Campus Marketplace" },
+    { name: "description", content: "Shop smarter with Campex. Discover the best campus deals and connect with trusted vendors on Campex, your ultimate marketplace for student essentials." },
+    { name: "keywords", content: "campus marketplace, student deals, vendor connections, student essentials, campus shopping, student discounts, trusted vendors" },
+  ]
+}
+
+export const action = async ({ request, }: Route.ActionArgs) => {
+  // TODO: Implement rate limiting to prevent abuse
+
+    const formData = await request.formData();
+    const userEmail = formData.get("email");
+    if (typeof userEmail !== "string" || userEmail.length === 0) {
+      return { error: "Email is required.", success: false };
+    }
+
+    // Send email to Brevo API
+    try {
+        if (!process.env.BREVO_API_KEY || !process.env.BREVO_LIST_ID) {
+            return { error: "Missing environment variables.", success: false };
+        }
+
+        const response = await fetch("https://api.brevo.com/v3/contacts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "api-key": `${process.env.BREVO_API_KEY}`
+            },
+            body: JSON.stringify({
+                email: userEmail,
+                listIds: [Number(process.env.BREVO_LIST_ID)],
+                updateEnabled: false
+            })
+        });
+
+        if (!response.ok) {
+            const responseJson = await response.json();
+
+            // If the user is already on the list, treat it as a success and inform them
+            if (responseJson.code === "duplicate_parameter") {
+                return { message: "You are already on the waitlist! Sit back and we'll be in touch", success: true };
+            }
+
+            console.error("Brevo API response:", await response.text());
+            return { error: "Failed to join the waitlist. Please try again later.", success: false };
+        }
+    } catch (err) {
+        console.error("Error contacting Brevo API:", err);
+        return { error: "Failed to join the waitlist. Please try again later.", success: false };
+    }
+
+    return { message: "Successfully joined the waitlist!", success: true };
+}
+
+export const LandingPage = ({actionData}: Route.ComponentProps) => {
   return (
-    <main>
-      {/* Nav bar */}
-      <Navbar />
+    <>
+    {/* Main Content */}
+      <main>
+        {/* Nav bar */}
+        <Navbar />
 
-      <section id='hero' className='relative pt-2 min-h-[55vh] lg:min-h-[70vh] bg-gradient-to-b from-background via-muted to-primary/30 overflow-visible pb-16 -mb-16'>
-        <div className='flex flex-col lg:flex-row'>
-          {/* Hero Content */}
-          <div className='px-6 sm:px-8 md:px-12 lg:px-[96.96px] flex flex-col h-full justify-center items-start space-y-6 lg:space-y-8 my-4 lg:my-20 sm:my-6'>
-            <div className='max-w-full lg:max-w-[646px]'>
-              <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-extrabold leading-tight sm:leading-[1.1] lg:leading-[60px] uppercase font-display'>Shop smarter with Campex</h1>
+        <section id='hero' className='relative pt-2 min-h-[55vh] lg:min-h-[70vh] bg-gradient-to-b from-background via-muted to-primary/30 overflow-visible pb-16 -mb-16'>
+          <div className='flex flex-col lg:flex-row'>
+            {/* Hero Content */}
+            <div className='px-6 sm:px-8 md:px-12 lg:px-[96.96px] flex flex-col h-full justify-center items-start space-y-6 lg:space-y-8 my-4 lg:my-20 sm:my-6'>
+              <div className='max-w-full lg:max-w-[646px]'>
+                <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-extrabold leading-tight sm:leading-[1.1] lg:leading-[60px] uppercase font-display'>Shop smarter with Campex</h1>
+              </div>
+              <div className='max-w-full lg:max-w-[546px]'>
+                <p className='text-base sm:text-lg md:text-xl lg:text-2xl text-foreground/70'>Discover the best campus deals and connect with trusted vendors on Campex, your ultimate marketplace for student essentials.</p>
+              </div>
+              <div>
+                <button className='bg-primary text-primary-foreground font-medium text-base md:text-[16px] px-4 py-2 md:px-[40px] md:py-4 rounded-full transition-colors flex items-center' onClick={() => {window.location.href = '#waitlist'}}>
+                  Get Started
+                  <FaArrowRightLong className='inline-block ml-4 text-primary-foreground' />
+                </button>
+              </div>
             </div>
-            <div className='max-w-full lg:max-w-[546px]'>
-              <p className='text-base sm:text-lg md:text-xl lg:text-2xl text-foreground/70'>Discover the best campus deals and connect with trusted vendors on Campex, your ultimate marketplace for student essentials.</p>
-            </div>
-            <div>
-              <button className='bg-primary text-primary-foreground font-medium text-base md:text-[16px] px-4 py-2 md:px-[40px] md:py-4 rounded-full transition-colors flex items-center' onClick={() => {window.location.href = '/register'}}>
-                Get Started
-                <FaArrowRightLong className='inline-block ml-4 text-primary-foreground' />
-              </button>
+
+            {/* Hero Image */}
+            <div className='mt-12 lg:mt-0 lg:ml-12 flex-1 flex justify-center px-6 sm:px-8 md:px-12 lg:px-0'>
+              <div className='relative flex w-full h-100 sm:h-80 lg:w-[480px] lg:h-[70vh]'>
+                <div className='bg-primary/80 translate-x-2 translate-y-2 rounded-3xl inset-0 absolute'></div>
+                <div className='bg-[url(/images/heroimage.png)] bg-cover bg-center w-full h-full relative rounded-3xl z-10'></div>
+
+                {/* 3d icons */}
+
+              </div>
             </div>
           </div>
 
-          {/* Hero Image */}
-          <div className='mt-12 lg:mt-0 lg:ml-12 flex-1 flex justify-center px-6 sm:px-8 md:px-12 lg:px-0'>
-            <div className='relative flex w-full h-100 sm:h-80 lg:w-[480px] lg:h-[70vh]'>
-              <div className='bg-primary/80 translate-x-2 translate-y-2 rounded-3xl inset-0 absolute'></div>
-              <div className='bg-[url(/images/heroimage.png)] bg-cover bg-center w-full h-full relative rounded-3xl z-10'></div>
+        </section>
 
-              {/* 3d icons */}
-
-            </div>
+        <section id='features' className='pt-16'>
+          <div>
+            <div className='text-2xl'> Featured Products </div>
           </div>
-        </div>
 
-      </section>
+        </section>
 
-      <section id='features' className='pt-16'>
-        <div>
-          <div className='text-2xl'> Featured Products </div>
-        </div>
 
-      </section>
-    </main>
+        <section id="waitlist" className='relative mx-4 sm:mx-6 md:mx-8 lg:mx-12 xl:mx-[102px] my-12 sm:my-16 md:my-20 rounded-2xl overflow-hidden'>
+
+          {/* Main container */}
+          <div className='relative overflow-hidden backdrop-blur-sm rounded-2xl sm:rounded-3xl md:rounded-4xl py-12 sm:py-14 md:py-16 lg:py-28 px-6 sm:px-8 md:px-12 lg:px-16 bg-tertiary/20'>
+
+            {/* Background Image */}
+            <img src="null" alt="Waitlist Background" className='absolute inset-0 w-full h-full object-cover object-center opacity-20 sm:opacity-30 md:opacity-50 -z-10 bg-transparent' />
+
+            {/* Actual content */}
+            { actionData?.success ? (
+              <div className='relative'>
+                <div className='flex flex-col'>
+                  <WaitlistSuccessCard />
+                </div>
+              </div>
+            ) : (<div className='relative mx-auto max-w-xl sm:max-w-2xl md:max-w-3xl space-y-6 sm:space-y-7 md:space-y-8 text-center'>
+              <div className='inline-block'>
+                <div className='bg-primary/20 rounded-full px-4 md:px-6 lg:px-8 py-2 sm:py-2.5 md:py-3 inline-flex items-center space-x-2'>
+                  <span className='text-primary text-base sm:text-lg md:text-xl font-medium'> Waitlist </span>
+                </div>
+              </div>
+              <h2 className='font-display text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold leading-tight'> Join Our Waitlist Today! </h2>
+              <p className=''> We’re building Nigeria’s first student + vendor marketplace.
+                  Join the waitlist and be the first to connect, sell, and grow when we launch. </p>
+
+              <Form method='post' className='max-w-full sm:max-w-lg md:max-w-2xl mx-auto mt-6 sm:mt-8 md:mt-10 transition-all duration-200 ease-in-out'>
+                {/* Swap the container once the user succeeds */} (
+                  <div className='relative'>
+                  {actionData?.error && (<p className='text-red-500 text-sm'>{actionData.message}</p>) }
+                  <input type="email" name='email' placeholder='Enter your email address' required className='rounded-full w-full h-12 sm:h-13 md:h-14 lg:h-16 pl-4 sm:pl-5 md:pl-6 pr-28 border border-foreground/40 focus:outline-none focus:ring-2 focus:ring-foreground focus:border-transparent transition-all duration-200 ease-in-out'/>
+                  <div className='absolute right-1 top-1/2 transform -translate-y-1/2'>
+                    <button type='submit' className='bg-primary text-primary-foreground font-medium text-base md:text-[16px] px-4 py-2 md:px-[40px] md:py-4 rounded-full transition-colors flex items-center'>
+                      Join Waitlist
+                    </button>
+                  </div>
+                </div>
+              </Form>
+            </div>)}
+          </div>
+        </section>
+      </main>
+
+      {/* Footer */}
+      <footer id='footer' className='relative py-6 bg-footer-background text-footer-foreground'>
+        <Footer />
+      </footer>
+    </>
   )
 }
 
